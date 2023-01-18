f27407042fee80fd62b5cfc3914133f8e52bb077
Alexander <alexandr.s.trushnikov@gmail.com>
2023-01-18 20:20:32 +0500

    Added helpers for authentication

--- src/contacts/helpers/auth.py
+++ src/contacts/helpers/auth.py
@@ -1,2 +1,21 @@
-def username_exist(username: str) -> bool:
-    ...
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from contacts.models.db.entities import UserInDb
+from contacts.db.crud.users import get_user
+from .security import passwords_match
+
+
+async def authenticate_user(
+    username: str,
+    password: str, 
+    session: AsyncSession,
+) -> UserInDb | None:
+    user = await get_user(username, session)
+
+    if user is None:
+        return None
+
+    if not passwords_match(plain_pw=password, hashed_pw=user.hashed_password):
+        return None
+
+    return user
