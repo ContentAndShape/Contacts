9783f6913d3039bf946dd5420c66ab85e6d498ca
Alexander <alexandr.s.trushnikov@gmail.com>
2023-01-19 14:01:20 +0500

    Formatting

--- tests/test_api/test_contacts.py
+++ tests/test_api/test_contacts.py
@@ -9,7 +9,6 @@ from .. import model_generator
 
 @pytest.mark.usefixtures("create_tables")
 class TestCreate:
-    
     @pytest.mark.asyncio
     async def test_422(self, client: AsyncClient):
         user = model_generator.User()
@@ -23,22 +22,26 @@ class TestCreate:
             "access_token": token,
         }
         contact = model_generator.Contact(owner_id=user.id)
-        contact.phone_number += '11111'
+        contact.phone_number += "11111"
         contact.id = str(contact.id)
 
         # Invalid length
-        response = await client.post(url="/contacts", json=asdict(contact), headers=headers)
+        response = await client.post(
+            url="/contacts", json=asdict(contact), headers=headers
+        )
         assert response.status_code == 422
         assert "length" in response.json()["detail"]
 
         contact = model_generator.Contact(owner_id=user.id)
         contact.id = str(contact.id)
         phone_number_list = [*contact.phone_number]
-        phone_number_list[-1] = 't'
+        phone_number_list[-1] = "t"
         contact.phone_number = "".join(phone_number_list)
 
         # Invalid format
-        response = await client.post(url="/contacts", json=asdict(contact), headers=headers)
+        response = await client.post(
+            url="/contacts", json=asdict(contact), headers=headers
+        )
         assert response.status_code == 422
         assert "format" in response.json()["detail"]
 
@@ -60,7 +63,9 @@ class TestCreate:
         # Trying to create contact with existing phone_number
         contact.phone_number = contact_in_db.phone_number
 
-        response = await client.post(url="/contacts", json=asdict(contact), headers=headers)
+        response = await client.post(
+            url="/contacts", json=asdict(contact), headers=headers
+        )
         assert response.status_code == 409
         assert "already exist" in response.json()["detail"]
 
@@ -79,5 +84,7 @@ class TestCreate:
         contact = model_generator.Contact(owner_id=user.id)
         contact.id = str(contact.id)
 
-        response = await client.post(url="/contacts", json=asdict(contact), headers=headers)
+        response = await client.post(
+            url="/contacts", json=asdict(contact), headers=headers
+        )
         assert response.status_code == 201
