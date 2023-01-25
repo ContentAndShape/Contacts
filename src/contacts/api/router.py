15e72f42e22a16ae877bf74d1a7d9f3b9ca699c3
Alexander <alexandr.s.trushnikov@gmail.com>
2023-01-25 11:55:19 +0500

    Added router for users

--- src/contacts/api/router.py
+++ src/contacts/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from contacts.api import auth, contacts
+from contacts.api import auth, contacts, users
 
 
 router = APIRouter()
 router.include_router(auth.router, prefix="/auth")
 router.include_router(contacts.router, prefix="/contacts")
+router.include_router(users.router, prefix="/users")
