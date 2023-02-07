443361e75f500da88f842f58d444ccc3bf2063bb
Alexander <alexandr.s.trushnikov@gmail.com>
2023-02-07 16:22:06 +0500

    Reworked dependency for contacts existence check, moved contact ownership validation to dependencies

--- src/contacts/api/contacts.py
+++ src/contacts/api/contacts.py
@@ -18,6 +18,8 @@ from .dependencies.db import get_session
 from .dependencies.api import (
     process_jwt, 
     get_payload_from_jwt, 
+    validate_contact_ownership,
+    check_contact_existence,
     get_filter_params, 
     validate_phone_number,
 )
@@ -98,7 +100,12 @@ async def get_contacts(
 @router.put(
     "/{contact_id}", 
     status_code=204,
-    dependencies=[Depends(process_jwt), Depends(validate_phone_number)],
+    dependencies=[
+    Depends(process_jwt),
+    Depends(check_contact_existence),
+    Depends(validate_contact_ownership),
+    Depends(validate_phone_number),
+    ],
 )
 async def update_contact(
     contact_id: uuid.UUID,
@@ -106,19 +113,4 @@ async def update_contact(
     payload: PayloadData = Depends(get_payload_from_jwt),
     db_session: AsyncSession = Depends(get_session),
 ) -> None:
-    contact_to_update = await get_contact(id=contact_id, db_session=db_session)
-    if contact_to_update is None:
-        raise HTTPException(status_code=404, detail="contact not found")
-
-    if payload.role == UserRoleEnum.admin.value:
-        await update_contact_(db_session=db_session, id=contact_id, **request_contact.dict())
-
-    if payload.role == UserRoleEnum.user.value:
-        if not await contact_is_owned_by_user(
-            contact_id=contact_id, 
-            user_id=payload.sub, 
-            session=db_session
-        ):
-            raise HTTPException(status_code=403, detail="user does not own this contact")
-        else:
-            await update_contact_(db_session=db_session, id = contact_id, **request_contact.dict())
+    await update_contact_(db_session=db_session, id=contact_id, **request_contact.dict())
--- src/contacts/api/dependencies/api.py
+++ src/contacts/api/dependencies/api.py
@@ -7,9 +7,12 @@ from pydantic import EmailStr
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from contacts.helpers.security import ALGORITHM, validate_jwt
+from contacts.helpers.contacts import contact_is_owned_by_user
 from .db import get_session
 from contacts.db.crud.users import get_user
+from contacts.db.crud.contacts import get_contact
 from contacts.models.schemas.auth import Token, PayloadData
+from contacts.models.schemas.meta import UserRoleEnum
 from contacts.models.schemas.contacts import BaseContact
 from contacts.models.schemas.users import UserInCreate
 from contacts.models.schemas.meta import ContactsFilterParams
@@ -30,6 +33,24 @@ def process_jwt(request: Request, token: Token = Depends(oauth2_scheme)) -> None
     if token is None:
         raise HTTPException(status_code=400, detail="no token provided")
     validate_jwt(token, request.app.state.secret)
+
+
+async def validate_contact_ownership(
+    contact_id: str,
+    payload: PayloadData = Depends(get_payload_from_jwt), 
+    db_session: AsyncSession = Depends(get_session),
+) -> None:
+    """Checks if user has rights to interact with requested contact"""
+    if payload.role == UserRoleEnum.admin.value:
+        return
+    
+    if payload.role == UserRoleEnum.user.value:
+        if not await contact_is_owned_by_user(
+            contact_id=contact_id, 
+            user_id=payload.sub, 
+            session=db_session
+        ):
+            raise HTTPException(status_code=403, detail="User does not own this contact")
 
 
 def get_filter_params(
@@ -58,11 +79,18 @@ def validate_phone_number(request_contact: BaseContact) -> None:
     if len(request_contact.phone_number) != 11:
         raise HTTPException(status_code=422, detail="invalid phone number length")
     
+    # TODO try to int whole number
     for char in request_contact.phone_number:
         try:
             int(char)
         except ValueError:
             raise HTTPException(status_code=422, detail="invalid phone number format")
+
+
+async def check_contact_existence(contact_id: str, db_sesion = Depends(get_session)) -> None:
+    contact = await get_contact(id=contact_id, db_session=db_sesion)
+    if contact is None:
+        raise HTTPException(status_code=404, detail="Contact doesn't exist")
 
 
 #TODO optimize this
