d5a5680a4acd63449f3a33ea602c2b920b0a9436
Alexander <alexandr.s.trushnikov@gmail.com>
2023-02-12 20:20:17 +0500

    SQLAlchemy 2.0 tables upgrade

--- src/contacts/models/db/tables.py
+++ src/contacts/models/db/tables.py
@@ -1,45 +1,48 @@
+from typing import Set
+from uuid import UUID
+
 from sqlalchemy.orm import (
-    declarative_base, 
-    declared_attr,
+    Mapped,
+    mapped_column,
+    DeclarativeBase, 
     relationship,
+    declared_attr,
 )
 from sqlalchemy import (
     Column,
-    Integer,
     String,
     ForeignKey,
     Enum,
 )
-from sqlalchemy.dialects.postgresql import UUID
 
 from .metaclasses import RoleEnum
 
 
-class Base:
+class Base(DeclarativeBase):
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower() + "s"
 
 
-Base = declarative_base(cls=Base)
+# Base = declarative_base(cls=Base)
 
 
 class User(Base):
-    id = Column(Integer, primary_key=True)
-    contacts = relationship("Contact", back_populates="owner")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    contacts: Mapped[Set["Contact"]] = relationship(back_populates="owner")
     username = Column(String(100), unique=True, nullable=False)
     hashed_password = Column(String(100), nullable=False)
     role = Column(Enum(RoleEnum), nullable=False)
 
 
 class Contact(Base):
-    id = Column(UUID, primary_key=True)
-    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    owner = relationship("User", back_populates="contacts")
-    last_name = Column(String(100), nullable=False)
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped["User"] = relationship(back_populates="contacts")
+    last_name = Column(String(100))
     first_name = Column(String(100), nullable=False)
-    middle_name = Column(String(100), nullable=False)
+    middle_name = Column(String(100))
     organisation = Column(String(100))
     job_title = Column(String(100))
-    email = Column(String(100), nullable=False)
+    email = Column(String(100))
     phone_number = Column(String(100), nullable=False)
